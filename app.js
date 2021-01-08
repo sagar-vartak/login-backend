5620b9ec88ae0c0c6f232631bfe0a3ed6f7bf722
Trainee 18 <sagar.vartak@contentstack.com>
2021-01-08 12:13:27 +0530

    changed route path

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/user", router);
+app.use("/", router);
 
 app.listen(process.env.PORT || 3000, function () {
   console.log(
